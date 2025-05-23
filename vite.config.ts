c435c5e61e76c1b1914a2a6e5cf5dac990d8f295
o.kovalchuk <mail.to.kovalchuk@gmail.com>
2025-05-23 22:18:57 +0300

    chore: update pwa favicon

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,7 @@
 import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react-swc";
 import { defineConfig } from "vite";
-import { VitePWA } from 'vite-plugin-pwa'
-
+import { VitePWA } from "vite-plugin-pwa";
 
 export default defineConfig({
   plugins: [
@@ -15,33 +14,38 @@ export default defineConfig({
         runtimeCaching: [
           {
             urlPattern: /^https:\/\/localhost:5173\/.*/i,
-            handler: 'NetworkFirst',
+            handler: "NetworkFirst",
             options: {
-              cacheName: 'api-cache',
+              cacheName: "api-cache",
               networkTimeoutSeconds: 10,
               expiration: {
                 maxEntries: 50,
-                maxAgeSeconds: 60 * 60 * 24 // 24 hours
+                maxAgeSeconds: 60 * 60 * 24, // 24 hours
               },
               cacheableResponse: {
-                statuses: [0, 200]
-              }
-            }
-          }
-        ]
+                statuses: [0, 200],
+              },
+            },
+          },
+        ],
       },
       manifest: {
-        name: 'SetDone',
-        short_name: 'SetDone',
-        theme_color: '#ffffff',
+        name: "SetDone",
+        short_name: "SetDone",
+        theme_color: "#059669",
         icons: [
           {
-            src: '/vite.svg',
-            sizes: '192x192',
-            type: 'image/svg+xml'
-          }
-        ]
-      }
+            src: "/icon-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "/icon-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
+      },
     }),
     react(),
     tailwindcss(),
