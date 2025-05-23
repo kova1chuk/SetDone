19b00a2cb632e55ca9981bb2da3671f2c9751b2d
o.kovalchuk <mail.to.kovalchuk@gmail.com>
2025-05-23 15:50:19 +0300

    fix: reload page daily on change

--- src/pages/DailyRoutinePage.tsx
+++ src/pages/DailyRoutinePage.tsx
@@ -77,7 +77,7 @@ export default function DailyRoutinePage() {
     return Object.values(summaries);
   }, [logs, userExercises]);
 
-  if (exercisesLoading || logLoading) {
+  if ((exercisesLoading || logLoading) && !userExercises.length) {
     return (
       <div className="flex items-center justify-center h-64">
         <div className="animate-spin rounded-full h-12 w-12 border-b-2 border-blue-600"></div>
